d80a4f1ad51180dbaf8a583e9fd3caa04bf781eb
Muzjili <1679139025@qq.com>
2024-11-26 14:57:34 +0800

    Site updated: 2024-11-26 14:57:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/15/可执行文件的相关概念/可执行文件的相关概念/","2024/11/17/汇编语言基本知识/汇编语言基本知识/"];function toRandomPost(){
+var posts=["2024/11/17/汇编语言基本知识/汇编语言基本知识/","2024/11/15/可执行文件的相关概念/可执行文件的相关概念/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
